affbc00a777de85026c6fe3a4319e4e9956b39f4
s0m1k111 <spavel1207@gmail.com>
2026-04-29 15:53:58 +0300

    student

---
+++ lib/student_name.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+class StudentName extends StatelessWidget {
+  const StudentName({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'Петр Иванов',
+      style: TextStyle(
+        fontSize: 32,
+        fontWeight: FontWeight.bold,
+        color: Colors.white,
+      ),
+    );
+  }
+}
