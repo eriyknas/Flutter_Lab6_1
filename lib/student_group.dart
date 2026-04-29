d8e07a27f673a440651b597e59a5c601c2fc2ca6
s0m1k111 <spavel1207@gmail.com>
2026-04-29 15:55:55 +0300

    group

---
+++ lib/student_group.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class StudentGroup extends StatelessWidget {
+  const StudentGroup({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'Группа: ИСП-231',
+      style: TextStyle(
+        fontSize: 24,
+        color: Colors.white70,
+      ),
+    );
+  }
+}
